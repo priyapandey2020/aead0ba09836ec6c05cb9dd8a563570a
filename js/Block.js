e848a407c3e2f7e55cda5f5ad529697b2831c81f
priyapandey2020 <66728335+priyapandey2020@users.noreply.github.com>
2021-10-12 13:29:19 +0530

    Update Block.js

--- js/Block.js
+++ js/Block.js
@@ -13,13 +13,20 @@ class Block{
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
+
         var pos= this.body.position;
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        rectMode(CENTER);
-        rect(0,0,this.width, this.height);
-        pop();
+        imageMode(CENTER);
+
+        if(this.body.speed <3){
+          image(this.image,pos.x,pos.y,this.width, this.height);
+        }
+        else{
+          World.remove(world, this.body);
+          push();
+          this.visiblity = this.visiblity - 5;
+          tint(255,this.visiblity);
+          image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
+          pop();
+        }
       }
 }
